d86df40638b4cf861c9764d6c77759cb48fb7d2e
Vinicius Horta <viniciushorta@Air-de-Vinicius.netcabo.pt>
2021-03-24 15:43:37 +0000

    implemented the dynamic list

--- api/controllers/product.js
+++ api/controllers/product.js
@@ -3,22 +3,25 @@ module.exports = app => {
   const controller = {};
   const https = require('https');
 
-  https.get('https://embed.2b.uy/data/CITpmNqtQewVXcV2/data.json', (resp) => {
-    let data = '';
+  controller.listProducts = (req, res) => {
+    https.get(`https://embed.2b.uy/data/${req.query.productLine}/data.json`, (resp) => {
+      let data = '';
 
-    // A d of data has been received.
-    resp.on('data', (d) => {
-      data += d;
+      // A d of data has been received.
+      resp.on('data', (d) => {
+        data += d;
+      });
+
+      resp.on('end', () => {
+        productDB = JSON.parse(data);
+      });
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
     });
 
-    resp.on('end', () => {
-      productDB = JSON.parse(data);
-    });
-  }).on("error", (err) => {
-    console.log("Error: " + err.message);
-  });
-  
-  controller.listProducts = (req, res) => res.status(200).json(productDB.lett_products);
+
+    res.status(200).json(productDB.lett_products);
+  }
 
   return controller;
 }
